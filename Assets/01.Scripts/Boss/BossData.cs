86353aa4bf780ee7b6094c2afb1b576c8924292d
taeho0919 <taeho0919kim@gmail.com>
2026-06-25 23:30:39 +0900

    복구 78%

--- Assets/01.Scripts/Boss/BossData.cs
+++ Assets/01.Scripts/Boss/BossData.cs
@@ -10,11 +10,6 @@ public class BossData : ScriptableObject
 {
     public BossType bossType;
     public int[] patternOrder;
-    public BossJData bjd;
+   
 }
 
-[System.Serializable]
-public class BossJData
-{
-    public GameObject _bossjattack3;
-}
